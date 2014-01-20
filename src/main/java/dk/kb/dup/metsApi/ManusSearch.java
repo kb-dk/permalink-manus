97f4ec9d20283fe439580d214b1fc1fbeaed0674
slu <slu@fe10f3e7-649f-474f-83ac-cb2dac7ad41c>
2014-01-20 14:22:48 +0000

    added some code
    git-svn-id: svn+ssh://code-01.kb.dk/home/svn/repos/mets-api/trunk@14 fe10f3e7-649f-474f-83ac-cb2dac7ad41c

--- src/main/java/dk/kb/dup/metsApi/ManusSearch.java
+++ src/main/java/dk/kb/dup/metsApi/ManusSearch.java
@@ -8,9 +8,29 @@ public class ManusSearch
 
     private String manusId = "";
     private String session = "manussession";
+
+    private String username          = "manus";
+    private String password          = "ft6yh";
+    private String jdbcUri           = "jdbc:oracle:thin:@oracledb.kb.dk:1521:prod";
+    private java.sql.Connection conn = null;
+
   
     public ManusSearch()
     {
+	this.initConnection(this.user,this.password,this.jdbcUri);	
+    }
+
+    public ManusSearch(String user, String password, String jdbcUri) {
+	this.initConnection(user,password,jdbcUri);
+    }
+
+    private void initConnection(String user, String password, String jdbcUri) {
+	java.util.Properties connectionProps = new Properties();
+	connectionProps.put("user",     user);
+	connectionProps.put("password", password);
+
+	this.conn = DriverManager.getConnection(jdbcUri,connectionProps);
+
     }
    
     public Collection executeQuery(String query, int maximumRecords)
