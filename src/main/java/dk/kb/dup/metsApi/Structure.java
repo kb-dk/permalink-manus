d87a9fea5d1e09982c89b375e6367176ac060542
slu <slu@fe10f3e7-649f-474f-83ac-cb2dac7ad41c>
2016-04-01 12:14:06 +0000

    now with a javax.cache
    git-svn-id: svn+ssh://code-01.kb.dk/home/svn/repos/mets-api/trunk@28 fe10f3e7-649f-474f-83ac-cb2dac7ad41c

---
+++ src/main/java/dk/kb/dup/metsApi/Structure.java
@@ -0,0 +1,94 @@
+package dk.kb.dup.metsApi;
+
+import org.dom4j.Document;
+import javax.cache.*;
+import javax.cache.configuration.*;
+import javax.cache.Caching;
+import javax.cache.CacheManager;
+import javax.cache.Cache;
+import javax.cache.expiry.*;
+import javax.cache.spi.*;
+import static javax.cache.expiry.Duration.ONE_HOUR;
+
+public class Structure
+{
+
+    private static Structure struct    = null;
+
+    Cache<String, String> cache = null;
+    //Cache cache = null;
+
+    private Structure() {
+	//resolve a cache manager
+	CachingProvider cachingProvider = Caching.getCachingProvider();
+	CacheManager cacheManager = cachingProvider.getCacheManager();
+
+	//configure the cache
+	MutableConfiguration<String, String> config = new MutableConfiguration<>();
+        config.setStoreByValue(true)
+	    .setExpiryPolicyFactory(AccessedExpiryPolicy.factoryOf(ONE_HOUR))
+	    .setStatisticsEnabled(true);
+
+	//create the cache
+	this.cache = cacheManager.createCache("simpleCache", config);
+    }
+
+    public static Structure getInstance() {
+	if(struct == null) {
+	    struct = new Structure();
+	}
+	return struct;
+    }
+
+    public String mets(String app,String docId) {
+	//cache operations
+	String key = app + docId;
+
+	String val = this.cache.get(key);
+	if(val != null ) {
+	    return val;
+	} else {
+	    val = this.cook(app,docId).asXML();
+	    this.cache.put(key, val);
+	}
+	return val;
+    }
+    
+
+    private Document cook(String app,String docId) {
+
+	Document metsStruct = null;    
+
+	if(app.equalsIgnoreCase("manus")) {
+	    Manus manus = new Manus(docId);
+	    metsStruct = manus.getMets();
+	} else if(app.equalsIgnoreCase("lum")){
+	    Lum lum = new Lum(docId);
+	    metsStruct = lum.getMets();
+	} else if(app.equalsIgnoreCase("lum-proj")) {
+	    Lum lum = new Lum();
+	    metsStruct = lum.getProjectMets();
+	} else if(app.equalsIgnoreCase("musman")) {
+	    MusMan musman = new MusMan(docId);
+	    metsStruct = musman.getMets();
+	} else if(app.equalsIgnoreCase("musman-proj")) {
+	    MusMan musman = new MusMan();
+	    metsStruct = musman.getProjectMets();
+	} else if(app.equalsIgnoreCase("mus")) {
+	    try{
+		Integer.parseInt(docId);
+		Mus mus = new Mus(docId,"");
+		metsStruct = mus.getNodeMets();
+	    } catch(Exception e){
+		Mus mus = new Mus("",docId);
+		metsStruct = mus.getProjectMets();
+	    }
+	} else if(app.equalsIgnoreCase("musik")) {
+	    Musik musik = new Musik();
+	    metsStruct = musik.getProjectsMets();
+	}
+
+	return metsStruct;
+
+    }
+}
--- src/main/webapp/get-mets-metadata.jsp
+++ src/main/webapp/get-mets-metadata.jsp
@@ -12,35 +12,8 @@ if(request.getParameter("app") != null) {
   app = request.getParameter("app");
 }
 
-if(app.equalsIgnoreCase("manus")){
-  dk.kb.dup.metsApi.Manus manus = new dk.kb.dup.metsApi.Manus(docId);
-  structure = manus.getMets();
-}else if(app.equalsIgnoreCase("lum")){
-  dk.kb.dup.metsApi.Lum lum = new dk.kb.dup.metsApi.Lum(docId);
-  structure = lum.getMets();
-}else if(app.equalsIgnoreCase("lum-proj")){
-  dk.kb.dup.metsApi.Lum lum = new dk.kb.dup.metsApi.Lum();
-  structure = lum.getProjectMets();
-}else if(app.equalsIgnoreCase("musman")){
-  dk.kb.dup.metsApi.MusMan musman = new dk.kb.dup.metsApi.MusMan(docId);
-  structure = musman.getMets();
-}else if(app.equalsIgnoreCase("musman-proj")){
-  dk.kb.dup.metsApi.MusMan musman = new dk.kb.dup.metsApi.MusMan();
-  structure = musman.getProjectMets();
-}else if(app.equalsIgnoreCase("mus")){
- try{
-    Integer.parseInt(docId);
-    dk.kb.dup.metsApi.Mus mus = new dk.kb.dup.metsApi.Mus(docId,"");
-    structure = mus.getNodeMets();
-  }catch(Exception e){
-    dk.kb.dup.metsApi.Mus mus = new dk.kb.dup.metsApi.Mus("",docId);
-    structure = mus.getProjectMets();
-  }
-}else if(app.equalsIgnoreCase("musik")){
-  dk.kb.dup.metsApi.Musik musik = new dk.kb.dup.metsApi.Musik();
-  structure = musik.getProjectsMets();
-}
+dk.kb.dup.metsApi.Structure struct = dk.kb.dup.metsApi.Structure.getInstance();
 
-out.println(structure.asXML());
+out.println(struct.mets(app,docId));
 
 %>
